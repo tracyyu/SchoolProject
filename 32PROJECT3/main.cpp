0b93be688f3390202e54e08cbd6c88f09e953f27
Tracy Yu <tracyyu93@g.ucla.edu>
2017-05-23 11:08:55 -0700

    upload working with classes

---
+++ 32PROJECT3/main.cpp
@@ -0,0 +1,14 @@
+#include "Map.h"
+#include <string>
+#include <iostream>
+#include <cassert>
+using namespace std;
+
+int main()
+{
+        cout << "Enter test number: ";
+        int n;
+        cin >> n;
+        testone(n);
+        cout << "Passed" << endl;
+}
